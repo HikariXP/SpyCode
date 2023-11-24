9bf7b738b70cfdfc6c22c5b817f2814d3b61684d
CharSui <charsui@peropero.net>
2023-11-24 17:02:26 +0800

    测试提交-helloWorld

---
+++ Assets/ProjectAsset/Config/WordDefine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordDefine : MonoBehaviour
+{
+	public static string[] words = new []
+	{
+		"哇浪0",
+		"哇浪1",
+		"哇浪2",
+		"哇浪3",
+		"哇浪4",
+		"哇浪5",
+		"哇浪6",
+		"哇浪7",
+		"哇浪8",
+		"哇浪9",
+		"哇浪10",
+	};
+}
